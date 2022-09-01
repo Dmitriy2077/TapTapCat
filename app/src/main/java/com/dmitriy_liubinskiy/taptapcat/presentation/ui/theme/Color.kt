e43e0bade6451c036d44fd369c7645251de4e8ef
Lubinskiy Dmitriy <dimka00788@gmail.com>
2022-09-01 15:44:25 +0300

    Initial commit

---
+++ app/src/main/java/com/dmitriy_liubinskiy/taptapcat/presentation/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.dmitriy_liubinskiy.taptapcat.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+// Primary
+val Yellow900 = Color(0xFFff913e)
+val Orange800 = Color(0xFFee6002)
+val Red900 = Color(0xFFb42e00)
+
+// Secondary
+val Blue300 = Color(0xFF65c0ff)
+val Blue500 = Color(0xFF0290ee)
+val Blue800 = Color(0xFF0063bb)
