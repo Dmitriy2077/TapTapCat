b7fe5b04ead158f60aa022f8030efa9f7e6b24f9
Lubinskiy Dmitriy <dimka00788@gmail.com>
2023-03-27 15:14:45 +0300

    Added material3 library. Updated build.gradle files.

--- app/src/main/java/com/dmitriy_liubinskiy/taptapcat/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/dmitriy_liubinskiy/taptapcat/presentation/ui/theme/Theme.kt
@@ -1,30 +1,21 @@
 package com.dmitriy_liubinskiy.taptapcat.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Yellow900,
-    primaryVariant = Red900,
+    onPrimary = Red900,
     secondary = Blue500
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Orange800,
-    primaryVariant = Red900,
+    onPrimary = Red900,
     secondary = Blue500
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -36,7 +27,7 @@ fun TapTapCatTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composa
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
